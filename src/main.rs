e6802ad9dc467fdad17df36447e35f694722ea7c
sensei <manishkumar97430@gmail.com>
2024-11-20 15:27:52 +0530

    minor changes

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,4 @@ mod logger;
 mod watcher;
 fn main() {}
 
-pub struct FileWatcher {
-    watcher: RecommendedWatcher,
-}
 
-impl FileWatcher {
-    pub fn new(tx: Sender<notify::Result<Event>>) -> anyhow::Result<Self> {
-        let watcher = RecommendedWatcher::new(tx, Config::default())
-            .context("Failed to create file watcher")?;
-        Ok(Self { watcher })
-    }
-
-    pub fn watch(&mut self, path: &Path) -> anyhow::Result<()> {
-        self.watcher
-            .watch(path, notify::RecursiveMode::Recursive)
-            .context(format!("Faileed to match path : {}", path.display()))?;
-        Ok(())
-    }
-    
-}
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,10 +1,39 @@
 use anyhow::{Context, Result};
-use colored::*;
 use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
-use regex::Regex;
-use std::{
-    path::{Path, PathBuf},
-    sync::mpsc::Sender,
-};
+use std::path::Path;
+use std::sync::mpsc::Sender;
 
+/// Struct representing the file watcher
+pub struct FileWatcher {
+    watcher: RecommendedWatcher,
+}
+
+impl FileWatcher {
+    /// Creates a new instance of `FileWatcher`
+    ///
+    /// # Arguments
+    /// - `tx`: A channel sender for receiving file events
+    ///
+    /// # Returns
+    /// - `anyhow::Result<Self>`: The constructed `FileWatcher` instance or an error
+    pub fn new(tx: Sender<notify::Result<Event>>) -> Result<Self> {
+        let watcher = RecommendedWatcher::new(tx, Config::default())
+            .context("Failed to create file watcher")?;
+        Ok(Self { watcher })
+    }
+
+    /// Starts watching the specified path
+    ///
+    /// # Arguments
+    /// - `path`: The path to watch
+    ///
+    /// # Returns
+    /// - `anyhow::Result<()>`: `Ok` if watching starts successfully, otherwise an error
+    pub fn watch(&mut self, path: &Path) -> Result<()> {
+        self.watcher
+            .watch(path, RecursiveMode::Recursive)
+            .context(format!("Failed to watch path: {}", path.display()))?;
+        Ok(())
+    }
+}
 
